6372bedff9f88e9383893b9b78478c12cd41aa61
Ace Alrich <hex0@live.com>
2020-06-01 19:04:59 +0100

    removed

--- client/src/App.js
+++ client/src/App.js
@@ -50,70 +50,20 @@ class App extends Component {
   render() {
     return (
       <div className="main">
-        {this.state.isLoggedIn ? (
-          <div>
-            <div className="title">
-              <Text type="secondary" style={{ fontSize: "36px" }}>
-                Websocket Chat
-              </Text>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                paddingBottom: 50,
-              }}
-            >
-              {this.state.messages.map((message) => (
-                <Card
-                  key={message.msg}
-                  style={{
-                    width: 300,
-                    margin: "16px 4px 0 4px",
-                    alignSelf:
-                      this.state.userName === message.user
-                        ? "flex-end"
-                        : "flex-start",
-                  }}
-                  loading={false}
-                >
-                  <Meta
-                    avatar={
-                      <Avatar
-                        style={{ color: "#f56a00", backgroundColor: "#fde3cf" }}
-                      >
-                        {message.user[0].toUpperCase()}
-                      </Avatar>
-                    }
-                    title={message.user}
-                    description={message.msg}
-                  />
-                </Card>
-              ))}
-            </div>
-            <div className="bottom">
-              <Search
-                placeholder="input message and send"
-                enterButton="Send"
-                value={this.state.searchVal}
-                size="large"
-                onChange={(e) => this.setState({ searchVal: e.target.value })}
-                onSearch={(value) => this.onButtonClicked(value)}
-              />
-            </div>
+        <div>
+          <div className="title">
+            <Text type="secondary" style={{ fontSize: "36px" }}>
+              Websocket
+            </Text>
           </div>
-        ) : (
-          <div style={{ padding: "200px 40px" }}>
-            <Search
-              placeholder="Enter Username"
-              enterButton="Login"
-              size="large"
-              onSearch={(value) =>
-                this.setState({ isLoggedIn: true, userName: value })
-              }
-            />
-          </div>
-        )}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              paddingBottom: 50,
+            }}
+          ></div>
+        </div>
       </div>
     );
   }
